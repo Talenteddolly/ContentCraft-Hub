2e202b469af9b3032f6c43f4802190eccb0884b2
Madhurima Rawat <105432776+madhurimarawat@users.noreply.github.com>
2025-03-15 19:24:20 +0530

    Update vite.config.js

--- docs/vite.config.js
+++ docs/vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/ContentCraft-Hub/',  // ✅ This is what you need for GitHub Pages!
   plugins: [react()],
 })
